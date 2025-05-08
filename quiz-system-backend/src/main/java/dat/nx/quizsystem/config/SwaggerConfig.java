5113f43cbac01a6a3e45fbc20f4aea21b1e26543
datx24 <xuandat475@gmail.com>
2025-05-08 18:07:33 +0700

    cài đặt swageer config

---
+++ quiz-system-backend/src/main/java/dat/nx/quizsystem/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package dat.nx.quizsystem.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Quiz System API").version("1.0"))
+                .components(new Components()
+                        .addResponses("DefaultResponse", new ApiResponse()
+                                .content(new Content()
+                                        .addMediaType("application/json", new MediaType()))))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement()
+                        .addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new io.swagger.v3.oas.models.security.SecurityScheme()
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
