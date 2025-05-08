cf17623c5473818bfddf16483489a90adb38f99b
datx24 <xuandat475@gmail.com>
2025-05-08 18:07:05 +0700

    cài đặt AuthController

---
+++ quiz-system-backend/src/main/java/dat/nx/quizsystem/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package dat.nx.quizsystem.controllers;
+
+import dat.nx.quizsystem.dto.LoginRequest;
+import dat.nx.quizsystem.dto.LoginResponse;
+import dat.nx.quizsystem.dto.RegisterRequest;
+import dat.nx.quizsystem.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        userService.register(request);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        LoginResponse response = userService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
