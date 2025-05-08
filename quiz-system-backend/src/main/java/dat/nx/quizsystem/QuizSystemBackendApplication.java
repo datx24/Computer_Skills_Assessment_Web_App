0993a90d3e9998b573fa158ac5ade05a91a1e70d
datx24 <xuandat475@gmail.com>
2025-05-08 15:29:51 +0700

    Tạo dự án, cài maven pom, cấu hình để kết nối cơ sử diệu

---
+++ quiz-system-backend/src/main/java/dat/nx/quizsystem/QuizSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package dat.nx.quizsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizSystemBackendApplication.class, args);
+	}
+
+}
---
+++ quiz-system-backend/src/test/java/dat/nx/quizsystem/QuizSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dat.nx.quizsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
