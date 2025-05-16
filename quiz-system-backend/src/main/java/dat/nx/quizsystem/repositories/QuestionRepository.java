d507da7fe20fe8a90649c9a8f39da4786bf5689b
datx24 <xuandat475@gmail.com>
2025-05-16 20:23:42 +0700

    Cập nhật QuestionRepository.java
    // Lấy câu hỏi từ exam dựa trên examId

--- quiz-system-backend/src/main/java/dat/nx/quizsystem/repositories/QuestionRepository.java
+++ quiz-system-backend/src/main/java/dat/nx/quizsystem/repositories/QuestionRepository.java
@@ -2,9 +2,8 @@ package dat.nx.quizsystem.repositories;
 
 import dat.nx.quizsystem.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findByTopic(String topic);
+    List<Question> findByExamId(Long examId);  // Lấy câu hỏi từ exam dựa trên examId
 }
