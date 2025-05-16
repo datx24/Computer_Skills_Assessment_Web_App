6609e8c8fa09912e96fae15e885de611e5620840
datx24 <xuandat475@gmail.com>
2025-05-16 20:32:26 +0700

    Cài đặt StringListConverter
    Chuyển List<String> ➝ String (dạng "A|B|C").

---
+++ quiz-system-backend/src/main/java/dat/nx/quizsystem/converter/StringListConverter.java
@@ -0,0 +1,26 @@
+package dat.nx.quizsystem.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Converter
+public class StringListConverter implements AttributeConverter<List<String>, String> {
+
+    private static final String SPLIT_CHAR = "|";
+
+    @Override
+    public String convertToDatabaseColumn(List<String> attribute) {
+        if (attribute == null || attribute.isEmpty()) return "";
+        return String.join(SPLIT_CHAR, attribute);
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        if (dbData == null || dbData.isEmpty()) return List.of();
+        return Arrays.stream(dbData.split("\\|")).collect(Collectors.toList());
+    }
+}
+
