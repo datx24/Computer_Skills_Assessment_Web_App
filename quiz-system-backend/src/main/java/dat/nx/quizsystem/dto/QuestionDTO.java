7a74d358c862cb958842af3d13cd5f17d265b67c
datx24 <xuandat475@gmail.com>
2025-05-16 20:36:14 +0700

    Cài đặt QuestionDTO

---
+++ quiz-system-backend/src/main/java/dat/nx/quizsystem/dto/QuestionDTO.java
@@ -0,0 +1,42 @@
+package dat.nx.quizsystem.dto;
+
+import java.util.List;
+
+public class QuestionDTO {
+    private String content;
+    private List<String> options;
+    private String correctAnswer;
+    private Long examId;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public Long getExamId() {
+        return examId;
+    }
+
+    public void setExamId(Long examId) {
+        this.examId = examId;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
