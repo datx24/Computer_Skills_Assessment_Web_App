411ecd3f2fa57c905e9fe627bbccdf9dec981398
datx24 <xuandat475@gmail.com>
2025-05-16 20:16:10 +0700

    Cài đặt model Result.java
    - thêm thuộc tính, get set

---
+++ quiz-system-backend/src/main/java/dat/nx/quizsystem/models/Result.java
@@ -0,0 +1,64 @@
+package dat.nx.quizsystem.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Result")
+public class Result {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer userId;
+
+    @Column(nullable = false)
+    private Integer examId;
+
+    @Column(nullable = false)
+    private Integer score;
+
+    @Column(nullable = false)
+    private LocalDateTime completedAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getExamId() {
+        return examId;
+    }
+
+    public void setExamId(Integer examId) {
+        this.examId = examId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+}
